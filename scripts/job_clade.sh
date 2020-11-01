bdf156210b2f5586973ebaf250b39399c72fa23d
Rebecca Ju <eeb354_rj374@grace1.grace.hpc.yale.internal>
2020-11-01 15:19:41 -0500

    adding cluster script and output for Porites 18s iqtree analysis

---
+++ scripts/job_clade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --partition=eeb354
+#SBATCH --job-name=clade_iqtree
+#SBATCH --time=30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+module load IQ-TREE/1.6.12
+
+iqtree -s por.alignment.fasta -nt AUTO
