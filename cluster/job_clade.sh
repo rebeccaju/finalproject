430178fa19a875d8cf578999e1a8b2e5c85082d8
Rebecca Ju <eeb354_rj374@grace2.grace.hpc.yale.internal>
2020-11-02 13:16:53 -0500

    adding cluster output for new CO1 sequences

--- cluster/job_clade.sh
+++ cluster/job_clade.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --partition=eeb354
 #SBATCH --job-name=clade_iqtree
-#SBATCH --time=30:00
+#SBATCH --time=2:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 
 module load IQ-TREE/1.6.12
 
-iqtree -s por.co1.alignment.fasta -nt AUTO
+iqtree -s por.co1.alignment.fasta -bb 1000 -nt AUTO
